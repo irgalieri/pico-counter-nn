966468ca3606947299de3a8105177acc8aa297bf
Ignacio Rodrigo Galieri <irgalieri@gmail.com>
2022-07-31 10:38:37 -0500

    Add Pic Layout And Allow Change Pins (#2)

--- src/h595-shift-register-nn.c
+++ src/h595-shift-register-nn.c
@@ -14,31 +14,51 @@ const uint SEGCODE[] = {
     0b111001110
 };
 
-bool hc595_out_shift_register(void) {
-    gpio_put(PICO_STR_CLOCK_PIN, true);
-    gpio_put(PICO_STR_CLOCK_PIN, false);
+ShiftRegister shift_register_new(PicoPinConfig config) {
+    ShiftRegister *reg = malloc(sizeof(ShiftRegister));
+
+    reg->serial = config.Serial;
+    reg->registerClock = config.RegisterClock;
+    reg->StorageClock = config.StorageClock;
+
+    // Init Shit Regsiter Pins
+    gpio_init(reg->serial);
+    gpio_set_dir(reg->serial, GPIO_OUT);
+    
+    gpio_init(reg->registerClock);
+    gpio_set_dir(reg->registerClock, GPIO_OUT);
+    
+    gpio_init(reg->StorageClock);
+    gpio_set_dir(reg->StorageClock, GPIO_OUT);
+
+    return *reg;
+}
+
+bool hc595_out_shift_register(ShiftRegister* sr) {
+    gpio_put(sr->StorageClock, true);
+    gpio_put(sr->StorageClock, false);
     return true;
 }
 
-bool hc595_shift_bit(bool bit) {
-    gpio_put(PICO_SERIAL_PIN, bit);
-    gpio_put(PICO_SR_CLOCK_PIN, true);
-    gpio_put(PICO_SR_CLOCK_PIN, false);
-    gpio_put(PICO_SERIAL_PIN, false);
+bool hc595_shift_bit(ShiftRegister* sr, bool bit) {
+    gpio_put(sr->serial, bit);
+    gpio_put(sr->registerClock, true);
+    gpio_put(sr->registerClock, false);
+    gpio_put(sr->serial, false);
 }
 
-bool hc595_shift(uint data) {
+bool hc595_shift(ShiftRegister* sr, uint data) {
     int b = 0b0000001;
     while (b <= 128) {
-        ((b <<= 1) & data) ? hc595_shift_bit(true) : hc595_shift_bit(false);
+        ((b <<= 1) & data) ? hc595_shift_bit(sr, true) : hc595_shift_bit(sr, false);
     }
-    hc595_out_shift_register();
+    hc595_out_shift_register(sr);
     return true;
 }
 
-bool hc595_shift_segment(uint segment) {
+bool hc595_shift_segment(ShiftRegister* sr, uint segment) {
     uint base = 0b111111100;
     if (segment > 9) return false;
     if (segment % 2) base = 0b111111110;
-    return hc595_shift(SEGCODE[segment] & base);
+    return hc595_shift(sr, SEGCODE[segment] & base);
 }
